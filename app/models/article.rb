934cbac363b10ca2b58319fdfb94a984273cc429
FranziGrieser <franzi.grieser@gmail.com>
2019-04-01 21:17:59 +0200

    add validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
-
+  validates :title, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :description, presence: true, length: { minimum: 15, maximum: 1500 }
 end
