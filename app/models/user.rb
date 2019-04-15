e4ad7ed8cf95264a6ad96bb9a50e31225e566df0
FranziGrieser <franzi.grieser@gmail.com>
2019-04-15 21:53:58 +0200

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  validates :username,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 25 }
+  VALID_EMAIL_REGEX = string[/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i]
+  validate :email,
+            presence: true,
+            length: { maximum: 105 },
+            uniqueness: { case_sensitive: false },
+            format: { width: VALID_EMAIL_REGEX }
 end
