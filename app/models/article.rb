58757f8ead756c1d9bb42f8ec3635b49b1ec7798
FranziGrieser <franzi.grieser@gmail.com>
2019-04-15 22:13:39 +0200

    add debugger and user-articöes-association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, length: { minimum: 5, maximum: 50 }
   validates :description, presence: true, length: { minimum: 15, maximum: 1500 }
+  validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :articles
+  before_save { self.email = email.downcase }
   validates :username,
             presence: true,
             uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 25 }
   VALID_EMAIL_REGEX = string[/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i]
-  validate :email,
+  validates :email,
             presence: true,
             length: { maximum: 105 },
             uniqueness: { case_sensitive: false },
