7f4d8ed2c4c9e6d1b506f170e99f552b3d8c5589
FranziGrieser <franzi.grieser@gmail.com>
2019-04-17 22:00:08 +0200

    add bcyrpt, secure-password and attribute to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_195739) do
+ActiveRecord::Schema.define(version: 2019_04_17_195652) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_04_15_195739) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
