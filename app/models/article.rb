dc0324c655afa217b5e4d67c2bd97cb369a31381
FranziGrieser <franzi.grieser@gmail.com>
2019-05-19 20:06:14 +0200

    update description length

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,6 @@
 class Article < ApplicationRecord
   belongs_to :user
   validates :title, presence: true, length: { minimum: 5, maximum: 50 }
-  validates :description, presence: true, length: { minimum: 15, maximum: 1500 }
+  validates :description, presence: true, length: { minimum: 15, maximum: 15000 }
   validates :user_id, presence: true
 end
