19aebfdc973bc956c1a865e41814823e3ccf4403
FranziGrieser <franzi.grieser@gmail.com>
2019-05-19 20:03:53 +0200

    redirect to login after sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to Blog Again, #{@user.username}!"
-      redirect_to articles_path
+      redirect_to login_path
     else
       render "new"
     end
